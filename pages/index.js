d3114b40bed1e81f24f8fe66c9bfface5cadd5c2
sumonsheikh <sm.sumon117@gmail.com>
2021-02-28 00:01:43 +0600

    fetch article from api

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,26 @@
 import Head from 'next/head'
+import ArticleList from '../component/ArticleList'
 
 
-export default function Home() {
+export default function Home( {articles}) {
+  console.log(articles)
   return (
     <div>
       <Head>
         <title>NextJs WebDevs News App</title>
         <meta name="keyword" content=" web, programming, development"/>
       </Head>
-      <h1>Welcome to Home</h1>
+
+      <ArticleList articles={articles}/>
     </div>
   )
+}
+export const getStaticProps = async () =>{
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+ return{
+   props:{
+     articles,
+   }
+ }
 }
