4876e3377181d879dd577f478dd2e5f15e09f6b6
sumonsheikh <sm.sumon117@gmail.com>
2021-02-28 08:55:44 +0600

    create static paths with getStaticPaths

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,4 +1,5 @@
 import {useRouter} from 'next/router';
+import Link from 'next/link'
 
 const article = ({article}) => {
     // const router = useRouter()
@@ -7,17 +8,38 @@ const article = ({article}) => {
         <>
             <h1>{article.title}</h1>
             <p>{article.body}</p>
+            <br/>
+            <Link href="/">Go Back</Link>
         </>
     )
 }
-export const getServerSideProps = async(context)=>{
+// export const getServerSideProps = async(context)=>{
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+//     const article = await res.json()
+
+//     return {
+//         props:{
+//             article,
+//         }
+//     }
+// }
+export const getStaticProps = async(context) =>{
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
     const article = await res.json()
-
-    return {
+    return{
         props:{
             article,
-        }
+        },
+    }
+}
+export const getStaticPaths = async () =>{
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+    const articles = await res.json()
+    const ids = articles.map((article)=>article.id)
+    const paths = ids.map((id) =>({params:{id: id.toString() }}))
+    return{
+        paths,
+        fallback: false,
     }
 }
 export default article
